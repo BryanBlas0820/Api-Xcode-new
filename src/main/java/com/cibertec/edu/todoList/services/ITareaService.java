da9034df44414743206dc1f47eab3b13f731e96d
BryanBlas0820 <bryankrb@hotmail.com>
2025-12-23 15:29:55 -0500

    Rename method to list tasks by user

--- src/main/java/com/cibertec/edu/todoList/services/ITareaService.java
+++ src/main/java/com/cibertec/edu/todoList/services/ITareaService.java
@@ -19,6 +19,6 @@ public interface ITareaService {
     Tarea editarTarea(Long id, Tarea tarea);
     boolean eliminarTarea(Long id);
     boolean TareaSemanaCompletada(Long id);
-    List<Tarea> listarTareaPorUsuario(Long usuarioId);
+    List<Tarea> listarTareasPorUsuario(Long usuarioId);
     
 }
